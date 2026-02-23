b6214ea7be2eb5330393a44dc9df2628aa4eb4db
matheeshapr <matheeshap62@gmail.com>
2026-02-23 09:36:01 +0530

    Adds InterviewSlot entity to manage interviewer availability.

--- src/main/java/edu/icet/model/entity/InterviewSlot.java
+++ src/main/java/edu/icet/model/entity/InterviewSlot.java
@@ -1,4 +1,20 @@
 package edu.icet.model.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
 public class InterviewSlot {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Interviewer interviewer;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private boolean isBooked = false;
 }
