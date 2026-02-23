4579f21ea28525aaa6954cffe3b09169c4526207
matheeshapr <matheeshap62@gmail.com>
2026-02-23 09:35:33 +0530

    Configures InterviewerDTO with id, name, email, and department fields.

--- src/main/java/edu/icet/model/dto/InterviewerDTO.java
+++ src/main/java/edu/icet/model/dto/InterviewerDTO.java
@@ -1,4 +1,11 @@
 package edu.icet.model.dto;
 
+import lombok.Data;
+
+@Data
 public class InterviewerDTO {
+    private Long id;
+    private String name;
+    private String email;
+    private String department;
 }
