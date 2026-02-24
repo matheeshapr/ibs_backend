14cf7ef5115e68ba059b75f6617a8d9274e852d1
matheeshapr <matheeshap62@gmail.com>
2026-02-24 20:16:38 +0530

    Configures `BookingService` as a Spring service component.

--- src/main/java/edu/icet/service/BookingService.java
+++ src/main/java/edu/icet/service/BookingService.java
@@ -1,8 +1,11 @@
 package edu.icet.service;
 
 import edu.icet.model.dto.BookingDTO;
+import org.springframework.stereotype.Service;
 
+@Service
 public class BookingService {
+
     public String createBooking(BookingDTO bookingDTO) {
 
     }
