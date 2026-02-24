6e2c694e8e3f7cedc610e3bcf7d5642560af781c
matheeshapr <matheeshap62@gmail.com>
2026-02-24 20:12:27 +0530

    Adds `@Autowired` and `@RequestBody` to booking creation endpoint.

--- src/main/java/edu/icet/controller/BookingController.java
+++ src/main/java/edu/icet/controller/BookingController.java
@@ -2,14 +2,17 @@ package edu.icet.controller;
 
 import edu.icet.model.dto.BookingDTO;
 import edu.icet.service.BookingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 public class BookingController {
 
+    @Autowired
     private BookingService bookingService;
 
     @PostMapping("/create")
-    public String createNewBooking(BookingDTO bookingDTO){
+    public String createNewBooking(@RequestBody BookingDTO bookingDTO){
         return bookingService.createBooking(bookingDTO);
     }
 }
