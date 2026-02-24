0bf9d62363c7784a2efd8cb58e38ef83a5e79fcd
matheeshapr <matheeshap62@gmail.com>
2026-02-24 20:11:19 +0530

    Adds booking creation endpoint to BookingController.

--- src/main/java/edu/icet/controller/BookingController.java
+++ src/main/java/edu/icet/controller/BookingController.java
@@ -1,4 +1,15 @@
 package edu.icet.controller;
 
+import edu.icet.model.dto.BookingDTO;
+import edu.icet.service.BookingService;
+import org.springframework.web.bind.annotation.PostMapping;
+
 public class BookingController {
+
+    private BookingService bookingService;
+
+    @PostMapping("/create")
+    public String createNewBooking(BookingDTO bookingDTO){
+        return bookingService.createBooking(bookingDTO);
+    }
 }
