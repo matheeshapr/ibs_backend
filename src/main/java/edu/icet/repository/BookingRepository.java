fac8d8c29d6d924472b801be73e868a2afdbf48c
matheeshapr <matheeshap62@gmail.com>
2026-02-24 20:13:54 +0530

    Completes BookingRepository setup for booking data persistence.

--- src/main/java/edu/icet/repository/BookingRepository.java
+++ src/main/java/edu/icet/repository/BookingRepository.java
@@ -1,4 +1,7 @@
 package edu.icet.repository;
 
-public interface BookingRepository {
+import edu.icet.model.entity.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookingRepository extends JpaRepository<Booking, Long> {
 }
