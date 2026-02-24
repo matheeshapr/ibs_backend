2f1231fffa1ee648b208ffd8ee6381d1f72df09f
matheeshapr <matheeshap62@gmail.com>
2026-02-24 20:13:33 +0530

    Completes REST controller setup for booking creation endpoint.

--- src/main/java/edu/icet/controller/BookingController.java
+++ src/main/java/edu/icet/controller/BookingController.java
@@ -5,7 +5,11 @@ import edu.icet.service.BookingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/booking")
 public class BookingController {
 
     @Autowired
