2dafa3b64a0c55ebca7ae5385fb8b917b3c384cb
matheeshapr <matheeshap62@gmail.com>
2026-02-25 12:12:14 +0530

    Adds service method for retrieving all interview slots.

--- src/main/java/edu/icet/service/BookingService.java
+++ src/main/java/edu/icet/service/BookingService.java
@@ -1,12 +1,18 @@
 package edu.icet.service;
 
 import edu.icet.model.dto.BookingDTO;
+import edu.icet.model.dto.InterviewSlotDTO;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BookingService {
 
     public String createBooking(BookingDTO bookingDTO) {
+        return null;
+    }
 
+    public List<InterviewSlotDTO> getAllSlots() {
     }
 }
