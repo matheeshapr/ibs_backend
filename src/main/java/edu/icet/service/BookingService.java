c66fdf180ae9f29951cfff7ea40a2a0ea8b1bf88
matheeshapr <matheeshap62@gmail.com>
2026-02-25 12:20:30 +0530

    Adds repository dependencies to BookingService.

--- src/main/java/edu/icet/service/BookingService.java
+++ src/main/java/edu/icet/service/BookingService.java
@@ -2,12 +2,23 @@ package edu.icet.service;
 
 import edu.icet.model.dto.BookingDTO;
 import edu.icet.model.dto.InterviewSlotDTO;
+import edu.icet.repository.BookingRepository;
+import edu.icet.repository.CandidateRepository;
+import edu.icet.repository.InterviewSlotRepository;
+import edu.icet.repository.InterviewerRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class BookingService {
+
+
+    private BookingRepository bookingRepository;
+
+    private InterviewSlotRepository interviewSlotRepository;
+
+    private CandidateRepository candidateRepository;
 
     public String createBooking(BookingDTO bookingDTO) {
         return null;
