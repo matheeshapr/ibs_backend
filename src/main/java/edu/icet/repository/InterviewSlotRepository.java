071a1e9cf9ae72f098c3b28e90f69c2243c28b22
matheeshapr <matheeshap62@gmail.com>
2026-02-25 12:37:02 +0530

    Adds query to find InterviewSlots by interviewer ID.

--- src/main/java/edu/icet/repository/InterviewSlotRepository.java
+++ src/main/java/edu/icet/repository/InterviewSlotRepository.java
@@ -3,5 +3,8 @@ package edu.icet.repository;
 import edu.icet.model.entity.InterviewSlot;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface InterviewSlotRepository extends JpaRepository<InterviewSlot,Long> {
+    List<InterviewSlot> findByInterviewerId(Long inteviewerId);
 }
