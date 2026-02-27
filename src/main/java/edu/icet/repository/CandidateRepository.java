7680653922f6851d499f59e766b1c7e95e3d62df
matheeshapr <matheeshap62@gmail.com>
2026-02-27 11:35:34 +0530

    Adds `@Repository` annotation to `CandidateRepository`.

--- src/main/java/edu/icet/repository/CandidateRepository.java
+++ src/main/java/edu/icet/repository/CandidateRepository.java
@@ -2,6 +2,8 @@ package edu.icet.repository;
 
 import edu.icet.model.entity.Candidate;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CandidateRepository extends JpaRepository<Candidate, Long> {
 }
