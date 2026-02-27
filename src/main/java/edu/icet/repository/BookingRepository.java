6bb62371deb0e15a5cb1274f509a251a7cf4aec5
matheeshapr <matheeshap62@gmail.com>
2026-02-27 11:34:26 +0530

    Adds `@Repository` annotation to `BookingRepository`.

--- src/main/java/edu/icet/repository/BookingRepository.java
+++ src/main/java/edu/icet/repository/BookingRepository.java
@@ -2,6 +2,8 @@ package edu.icet.repository;
 
 import edu.icet.model.entity.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookingRepository extends JpaRepository<Booking, Long> {
 }
