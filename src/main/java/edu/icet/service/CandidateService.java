17f6358abb6bc25b67b40f487bb7bc800abf3284
matheeshapr <matheeshap62@gmail.com>
2026-02-27 11:36:02 +0530

    Adds `CandidateService` for managing candidate entity data.

--- src/main/java/edu/icet/service/CandidateService.java
+++ src/main/java/edu/icet/service/CandidateService.java
@@ -1,4 +1,23 @@
 package edu.icet.service;
 
+import edu.icet.model.entity.Candidate;
+import edu.icet.repository.CandidateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CandidateService {
+
+    @Autowired
+    private CandidateRepository candidateRepository;
+
+    public Candidate save(Candidate candidate) {
+        return candidateRepository.save(candidate);
+    }
+
+    public List<Candidate> getAll() {
+        return candidateRepository.findAll();
+    }
 }
