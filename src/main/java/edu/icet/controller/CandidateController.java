8dab8438fe31189146438f6e2b4feef0887ae943
matheeshapr <matheeshap62@gmail.com>
2026-02-27 11:35:21 +0530

    java.net.http.HttpConnectTimeoutException: HTTP connect timed out

--- src/main/java/edu/icet/controller/CandidateController.java
+++ src/main/java/edu/icet/controller/CandidateController.java
@@ -1,4 +1,28 @@
 package edu.icet.controller;
 
+import edu.icet.model.entity.Candidate;
+import edu.icet.service.CandidateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/candidate")
+@CrossOrigin
 public class CandidateController {
+
+    @Autowired
+    private CandidateService candidateService;
+
+    @PostMapping("/add")
+    public Candidate addCandidate(@RequestBody Candidate candidate){
+        return candidateService.save(candidate);
+    }
+
+    @GetMapping("/get-all")
+    public List<Candidate> getAllCandidate(){
+        return candidateService.getAll();
+    }
 }
