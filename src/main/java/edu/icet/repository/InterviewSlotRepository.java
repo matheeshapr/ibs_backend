e79086ab6da918be252fb443421757b580b3f137
matheeshapr <matheeshap62@gmail.com>
2026-02-28 10:09:51 +0530

    Adds `@Repository` to `InterviewSlotRepository`.

--- src/main/java/edu/icet/repository/InterviewSlotRepository.java
+++ src/main/java/edu/icet/repository/InterviewSlotRepository.java
@@ -2,9 +2,11 @@ package edu.icet.repository;
 
 import edu.icet.model.entity.InterviewSlot;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface InterviewSlotRepository extends JpaRepository<InterviewSlot,Long> {
     List<InterviewSlot> findByInterviewerId(Long inteviewerId);
 }
