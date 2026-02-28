c6c90c464faa6bc8229dfcd131a1221069d1939e
matheeshapr <matheeshap62@gmail.com>
2026-02-28 10:09:25 +0530

    Adds InterviewerService for managing interviewer data.

--- src/main/java/edu/icet/service/InterviewerService.java
+++ src/main/java/edu/icet/service/InterviewerService.java
@@ -1,4 +1,23 @@
 package edu.icet.service;
 
+import edu.icet.model.entity.Interviewer;
+import edu.icet.repository.InterviewerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class InterviewerService {
+
+    @Autowired
+    private InterviewerRepository interviewerRepository;
+
+    public Interviewer save(Interviewer interviewer) {
+        return interviewerRepository.save(interviewer);
+    }
+
+    public List<Interviewer> getAll() {
+        return interviewerRepository.findAll();
+    }
 }
