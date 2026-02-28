95aab6c6d5b15395a7ee2a6701d639c5d0857350
matheeshapr <matheeshap62@gmail.com>
2026-02-28 10:10:02 +0530

    Adds InterviewSlotService for managing interview slot data.

--- src/main/java/edu/icet/service/InterviewSlotService.java
+++ src/main/java/edu/icet/service/InterviewSlotService.java
@@ -1,4 +1,24 @@
 package edu.icet.service;
 
+import edu.icet.model.entity.InterviewSlot;
+import edu.icet.repository.InterviewSlotRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class InterviewSlotService {
+
+    @Autowired
+    private InterviewSlotRepository interviewSlotRepository;
+
+    public InterviewSlot saveSlot(InterviewSlot interviewSlot) {
+        return interviewSlotRepository.save(interviewSlot);
+
+    }
+
+    public List<InterviewSlot> getAllSlots() {
+        return interviewSlotRepository.findAll();
+    }
 }
