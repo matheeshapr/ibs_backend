bfa7a3b3f0d2c23178cacdf62577d653af37ce5a
matheeshapr <matheeshap62@gmail.com>
2026-02-28 10:08:37 +0530

    Adds `InterviewerController` for managing interviewer data.

--- src/main/java/edu/icet/controller/InterviewerController.java
+++ src/main/java/edu/icet/controller/InterviewerController.java
@@ -1,4 +1,27 @@
 package edu.icet.controller;
 
+import edu.icet.model.entity.Interviewer;
+import edu.icet.service.InterviewerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/interviewer")
+@CrossOrigin
 public class InterviewerController {
+
+    @Autowired
+    private InterviewerService interviewerService;
+
+    @PostMapping("/add")
+    public Interviewer addInterviewer(@RequestBody Interviewer interviewer){
+        return interviewerService.save(interviewer);
+    }
+
+    @GetMapping("/get-all")
+    public List<Interviewer> getAllInterviewer(){
+        return interviewerService.getAll();
+    }
 }
