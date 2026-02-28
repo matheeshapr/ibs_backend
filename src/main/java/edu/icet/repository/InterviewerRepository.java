b6a25b5104d2813ff2e4d4b48a0873695bb9b707
matheeshapr <matheeshap62@gmail.com>
2026-02-28 10:09:09 +0530

    Adds `@Repository` to `InterviewerRepository`.

--- src/main/java/edu/icet/repository/InterviewerRepository.java
+++ src/main/java/edu/icet/repository/InterviewerRepository.java
@@ -2,6 +2,8 @@ package edu.icet.repository;
 
 import edu.icet.model.entity.Interviewer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface InterviewerRepository extends JpaRepository<Interviewer, Long> {
 }
