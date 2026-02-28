425e884aa5ce4c975d929c4d0e7eed24fd258578
matheeshapr <matheeshap62@gmail.com>
2026-02-28 10:09:38 +0530

    Adds InterviewSlotController for managing interview slot data.

--- src/main/java/edu/icet/controller/InterviewSlotController.java
+++ src/main/java/edu/icet/controller/InterviewSlotController.java
@@ -1,4 +1,29 @@
 package edu.icet.controller;
 
+import edu.icet.model.entity.InterviewSlot;
+import edu.icet.service.InterviewSlotService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/interview-slot")
+@CrossOrigin
 public class InterviewSlotController {
+
+    @Autowired
+    private InterviewSlotService interviewSlotService;
+
+    @PostMapping("/add")
+    public InterviewSlot addSlot(@RequestBody InterviewSlot interviewSlot){
+        return interviewSlotService.saveSlot(interviewSlot);
+    }
+
+    @GetMapping("/all-slot")
+    public List<InterviewSlot> getAllSlots(){
+        System.out.println(interviewSlotService.getAllSlots());
+        return interviewSlotService.getAllSlots();
+    }
+
 }
