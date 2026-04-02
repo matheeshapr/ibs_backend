5722c31c800fa5e0f70b25b8614318e93c659844
matheeshapr <matheeshap62@gmail.com>
2026-04-02 09:52:37 +0530

    Restricts cross-origin requests in BookingController to specific origin

--- src/main/java/edu/icet/controller/BookingController.java
+++ src/main/java/edu/icet/controller/BookingController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/booking")
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:4200")
 public class BookingController {
 
     @Autowired
