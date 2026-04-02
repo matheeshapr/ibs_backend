18bb5ded26841e3e0368b1b991a941090632df7d
matheeshapr <matheeshap62@gmail.com>
2026-04-02 09:53:06 +0530

    Restricts cross-origin requests in InterviewerController to specific origin

--- src/main/java/edu/icet/controller/InterviewerController.java
+++ src/main/java/edu/icet/controller/InterviewerController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/interviewer")
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:4200")
 public class InterviewerController {
 
     @Autowired
