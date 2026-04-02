c585c146fb9fbe2ee64124dd581615c535ec09a9
matheeshapr <matheeshap62@gmail.com>
2026-04-02 09:52:46 +0530

    Restricts cross-origin requests in CandidateController to specific origin

--- src/main/java/edu/icet/controller/CandidateController.java
+++ src/main/java/edu/icet/controller/CandidateController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/candidate")
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:4200")
 public class CandidateController {
 
     @Autowired
