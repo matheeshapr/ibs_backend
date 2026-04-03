ed72a2fc32c891ade184c798546c1a2dd703ff0c
matheeshapr <matheeshap62@gmail.com>
2026-04-03 08:54:14 +0530

    Adds registerCandidate method to CandidateService for candidate registration

--- src/main/java/edu/icet/service/CandidateService.java
+++ src/main/java/edu/icet/service/CandidateService.java
@@ -1,6 +1,8 @@
 package edu.icet.service;
 
+import edu.icet.model.dto.CandidateDTO;
 import edu.icet.model.entity.Candidate;
+import edu.icet.model.entity.User;
 import edu.icet.repository.CandidateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,5 +21,18 @@ public class CandidateService {
 
     public List<Candidate> getAll() {
         return candidateRepository.findAll();
+    }
+
+    public void registerCandidate(CandidateDTO dto) {
+        User user = new User();
+        user.setEmail(dto.getEmail());
+        user.setPassword(dto.getPassword());
+        user.setRole("CANDIDATE");
+
+        Candidate candidate = new Candidate();
+        candidate.setName(dto.getName());
+        candidate.setUser(user);
+
+        candidateRepository.save(candidate);
     }
 }
