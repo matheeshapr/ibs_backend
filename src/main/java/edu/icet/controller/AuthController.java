25278cc340684a407ccad0872eaedd8569d3a88b
matheeshapr <matheeshap62@gmail.com>
2026-04-05 09:20:25 +0530

    Adds login endpoint to AuthController for user authentication

--- src/main/java/edu/icet/controller/AuthController.java
+++ src/main/java/edu/icet/controller/AuthController.java
@@ -1,4 +1,29 @@
 package edu.icet.controller;
 
+import edu.icet.model.dto.LoginDTO;
+import edu.icet.model.entity.User;
+import edu.icet.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@CrossOrigin(origins = "http://localhost:4200")
 public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginDTO loginDto) {
+        User user = authService.login(loginDto);
+
+        if (user != null) {
+            return ResponseEntity.ok(user);
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid Email or Password");
+        }
+    }
 }
