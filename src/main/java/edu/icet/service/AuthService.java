2bd6b41735a976f9bf95c50f2bdf5a30cb03d43e
matheeshapr <matheeshap62@gmail.com>
2026-04-06 19:22:19 +0530

    Implements login method in AuthService for user authentication

--- src/main/java/edu/icet/service/AuthService.java
+++ src/main/java/edu/icet/service/AuthService.java
@@ -1,4 +1,28 @@
 package edu.icet.service;
 
+import edu.icet.model.dto.LoginDTO;
+import edu.icet.model.entity.User;
+import edu.icet.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class AuthService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User login(LoginDTO loginDto) {
+        Optional<User> userOptional = Optional.ofNullable(userRepository.findByEmail(loginDto.getEmail()));
+
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+            if (user.getPassword().equals(loginDto.getPassword())) {
+                return user;
+            }
+        }
+        return null;
+    }
 }
